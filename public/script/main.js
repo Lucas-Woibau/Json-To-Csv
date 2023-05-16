e87e961c4a9c8f255b29848b8d689fe6af3c7c7b
Lucas Woibau <lucaswoibau7@gmail.com>
2023-05-16 16:38:24 -0300

    Implementando blob

--- public/script/main.js
+++ public/script/main.js
@@ -1,17 +1,39 @@
 const input = document.getElementById('btn-converter')
+
 const btnUpload = document.getElementById('inp-upload')
 const btnDownload = document.getElementById('btn-download')
-const preview = document.getElementById('csv-text-file')
 
-btnUpload.addEventListener('click', function(){
+const preview1 = document.getElementById('json-text-file')
+const preview2 = document.getElementById('csv-text-file')
+
+btnUpload.addEventListener('change', function(){
     const archive = this.files[0]
     const reader = new FileReader()
 
     reader.addEventListener('load', function(){
-        console.log(reader.result)
+        preview1.value = reader.result
     })
 
     if(archive){
         reader.readAsText(archive)
     }
+})
+
+const download = function(){
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    document.body.appendChild(a)
+
+    return function(content, archiveName){
+        const blob = new Blob([content],{type:'octet/stream'})
+        const url = window.URL.createObjectURL(blob)
+        a.href = url
+        a.download = archiveName
+        a.click()
+        window.URL.revokeObjectURL(url)
+    }
+}
+
+btnDownload.addEventListener('click', function(){
+    download()(preview1.value,'teste.txt')
 })
