86ed9a6d62fbb62a4d1bbcb86814631a70df5043
Lucas Woibau <lucaswoibau@hotmail.com>
2023-05-15 21:08:54 -0300

    Adicionando visual commit 2

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,17 @@
-const input = document.getElementById('btn-download')
+const input = document.getElementById('btn-converter')
+const btnUpload = document.getElementById('inp-upload')
+const btnDownload = document.getElementById('btn-download')
+const preview = document.getElementById('csv-text-file')
 
-input.addEventListener('click', function(){
-    console.log("Botao clicado")
+btnUpload.addEventListener('click', function(){
+    const archive = this.files[0]
+    const reader = new FileReader()
+
+    reader.addEventListener('load', function(){
+        console.log(reader.result)
+    })
+
+    if(archive){
+        reader.readAsText(archive)
+    }
 })
