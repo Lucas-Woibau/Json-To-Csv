7136592be60890a78313b94fd6b29fbbb8a72b8b
Lucas Woibau <lucaswoibau7@gmail.com>
2023-05-17 16:44:42 -0300

    Lógica de conversão implantada

--- public/script/main.js
+++ public/script/main.js
@@ -1,45 +1,79 @@
-const input = document.getElementById('btn-converter')
-
-const btnUpload = document.getElementById('inp-upload')
-const btnDownload = document.getElementById('btn-download')
-
 const preview1 = document.getElementById('json-text-file')
 const preview2 = document.getElementById('csv-text-file')
 
+const fileName1 = document.getElementById('file-name-1')
+const fileName2 = document.getElementById('file-name-2')
+
+const btnUpload = document.getElementById('inp-upload')
 // Adicionando um ouvinte de evento para o botão de upload de arquivo
 btnUpload.addEventListener('change', function(){
-    const archive = this.files[0]
+    const file = this.files[0]
     const reader = new FileReader() 
-
+    const fileName = file.name
+    
     // Adicionando um ouvinte de evento para quando a leitura do arquivo estiver concluída
     reader.addEventListener('load', function(){
-        preview1.value = reader.result 
+        preview1.value = reader.result
+        fileName1.value = fileName
     })
 
-    if(archive){
-        reader.readAsText(archive) 
+    if(file){
+        reader.readAsText(file) 
     }
 })
 
-// Função de download de arquivo
-const download = function() {
-    const a = document.createElement('a'); // Criando um elemento <a> (link)
-    a.style.display = 'none'; // Ocultando o link
+const btnConverter = document.getElementById('btn-converter')
+// Função de coverter JSON para CSV
+function ConvertJSONtoCSV(jsonData){
+    const jsonArray = JSON.parse(jsonData)
 
-    document.body.appendChild(a); // Adicionando o link ao corpo do documento
+    // Obter as chaves do objeto JSON como cabeçalhos do CSV
+    const headers = Object.keys(jsonArray[0])
 
-    return function(content, archiveName) {
-        const blob = new Blob([content], { type: 'octet/stream' }); // Criando um objeto Blob com o conteúdo do arquivo
-        const url = window.URL.createObjectURL(blob); // Criando uma URL para o objeto Blob
-        a.href = url; // Definindo a URL do link para a URL do objeto Blob
-        a.download = archiveName; // Definindo o nome do arquivo de download
-        a.click(); // Simulando um clique no link
-        window.URL.revokeObjectURL(url); // Liberando a URL do objeto Blob
+    // Criar as linhas de dados do CSV
+    const rows = jsonArray.map((obj) => headers.map((header) => obj[header]))
+
+    // Combinar cabeçalhos e linhas em um único array
+    const csvData = [headers, ...rows]
+
+    // Converter o array em uma string CSV
+    const csvContent = csvData.map((row) => row.join(',')).join('\n')
+
+    return csvContent;
+}
+
+
+function downloadCSV(csvContent, fileName) {
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', fileName);
+    link.click();
+    window.URL.revokeObjectURL(link)
+}
+
+function Convert(){
+    try {
+        preview2.value = ConvertJSONtoCSV(preview1.value)     
+    } catch (error) {
+        alert(error + " Não é um JSON válido")
     }
-};
+} 
 
-// Adicionando um ouvinte de evento para o botão de download de arquivo
-btnDownload.addEventListener('click', function() {
-    const fileName = document.getElementById('file-name').value;
-    download()(preview1.value, `${fileName}.txt`); // Realizando o download do conteúdo do campo de visualização JSON
-});
+const btnDownload = document.getElementById('btn-download')
+btnDownload.addEventListener('click', function(){
+    const fileName1 = document.getElementById('file-name-1').value.split('.')[0]
+    const fileName = fileName2.value.split('.')[0]; 
+    
+    if(fileName){
+        downloadCSV(preview2.value, fileName + `.csv`);
+    }
+    else if(fileName1){
+        downloadCSV(preview2.value, fileName1 + `.csv`);
+    }
+    else{
+        alert('Please enter the new file name')
+    }
+})
+
