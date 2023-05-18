5d431112e0b864d56d920233ed576dc011529243
Lucas Woibau <lucaswoibau@hotmail.com>
2023-05-18 07:47:50 -0300

    Adicionando o botao de limpar

--- public/script/main.js
+++ public/script/main.js
@@ -13,7 +13,11 @@ btnUpload.addEventListener('change', function(){
     
     // Adicionando um ouvinte de evento para quando a leitura do arquivo estiver concluída
     reader.addEventListener('load', function(){
-        preview1.value = reader.result
+        if(preview1){
+            preview1.value = reader.result
+        }else{
+            preview1.value = new reader.result
+        }
         fileName1.value = fileName
     })
 
@@ -42,7 +46,7 @@ function ConvertJSONtoCSV(jsonData){
     return csvContent;
 }
 
-
+// Implantando download do arquivo csv
 function downloadCSV(csvContent, fileName) {
     const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
@@ -53,6 +57,7 @@ function downloadCSV(csvContent, fileName) {
     window.URL.revokeObjectURL(link)
 }
 
+// Funcionalidade do botao converter e verificando se o JSON é válido
 function Convert(){
     try {
         preview2.value = ConvertJSONtoCSV(preview1.value)     
@@ -66,14 +71,59 @@ btnDownload.addEventListener('click', function(){
     const fileName1 = document.getElementById('file-name-1').value.split('.')[0]
     const fileName = fileName2.value.split('.')[0]; 
     
+    // Caso o novo nome do arquivo fique em branco, ele pega o nome do arquivo original
     if(fileName){
-        downloadCSV(preview2.value, fileName + `.csv`);
+        downloadCSV(preview2.value, fileName + `Converted.csv`);
     }
     else if(fileName1){
-        downloadCSV(preview2.value, fileName1 + `.csv`);
+        downloadCSV(preview2.value, fileName1 + `Converted.csv`);
     }
     else{
         alert('Please enter the new file name')
     }
 })
 
+// Adicionando a funcao de copiar junto com as animacoes
+const copy = document.getElementById('btn-copy')
+copy.addEventListener('click', function(){
+    const preview2 = document.getElementById('csv-text-file').value
+    const copiedValue = `${preview2}`
+
+    navigator.clipboard.writeText(copiedValue.valueOf())
+
+    .then(() =>{
+        copy.textContent = `Copied`
+        setTimeout(() => {
+            copy.textContent = `Copy`
+        }, 2000);
+    })
+    .catch((error) => {
+        alert(error)
+    })
+})
+
+// Implementado a funcao de past/colar
+const past = document.getElementById('btn-past')
+past.addEventListener('click',() => {
+    if(navigator.clipboard){
+        navigator.clipboard.readText()
+        .then(function(content){
+            if(content){
+                preview1.value = content
+            }
+            else{
+                console.log("Empty value")
+            }
+        }).catch(function(error){
+            alert(error)
+        })
+    }else{
+        alert("The navigator does not support this API")
+    }
+})
+
+// Implementando a funcao de clear/limpar
+const clear = document.getElementById('btn-clear')
+clear.addEventListener('click', function(){
+    
+})
